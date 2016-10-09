0b7785bbeb22e9d8b141c84c549df464ef0ed9fa
Joseph Arhar <josepharhar@gmail.com>
2016-10-09 12:20:18 -0700

    websockets progress

--- sockets/src/main/resources/static/app.js
+++ sockets/src/main/resources/static/app.js
@@ -1,5 +1,5 @@
 console.log('creating websocket');
-var socket = new WebSocket('/foo');
+var socket = new WebSocket('ws://127.0.0.1:8080/foo');
 
 socket.onopen = function(event) {
   console.log('socket.onopen');
